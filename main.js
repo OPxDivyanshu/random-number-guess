c41f9fd86f8a7e36355f659ecb1ad60449e5ad74
Divyanshu <95847996+OPxDivyanshu@users.noreply.github.com>
2024-04-17 20:48:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const random = Math.floor(Math.random() * 10);
+let random = generateRandomNumber(); // Initial random number
+
 const inputBT = document.getElementById("input");
 const buttonBT = document.getElementById("button");
 const resultBT = document.getElementById("result");
@@ -11,4 +12,15 @@ buttonBT.addEventListener('click', function() {
     } else {
         resultBT.innerHTML = "Wrong! The correct answer was " + random + ". Try again!";
     }
+    
+   
+    inputBT.value = "";
+    
+   
+    random = generateRandomNumber();
+    console.log(random);
 });
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10);
+}
